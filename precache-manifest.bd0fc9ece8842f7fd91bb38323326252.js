c0a488e5fb4afbee8dde660992dcd79b6c691f50
absmohamed <cabdullahidaud@Hotmail.com>
2019-11-20 11:56:59 +1000

    Updates

---
+++ precache-manifest.bd0fc9ece8842f7fd91bb38323326252.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60867908f4fa46487057d1962e41c4a",
+    "url": "/blog-client/index.html"
+  },
+  {
+    "revision": "7e23c8f691d4fb0e2c62",
+    "url": "/blog-client/static/css/2.46f0bf41.chunk.css"
+  },
+  {
+    "revision": "7e23c8f691d4fb0e2c62",
+    "url": "/blog-client/static/js/2.0a48bea9.chunk.js"
+  },
+  {
+    "revision": "62852493e146ce044077",
+    "url": "/blog-client/static/js/main.7d450ccd.chunk.js"
+  },
+  {
+    "revision": "0a2fb8f7e4203d6b14ed",
+    "url": "/blog-client/static/js/runtime-main.2689d353.js"
+  }
+]);
